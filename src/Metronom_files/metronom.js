f758323bce96324ef0fd0f6978c614c8aa7fab12
Christoph Raaflaub <raaflaub@puzzle.ch>
2018-12-10 17:35:09 +0100

    format and clean code

--- src/Metronom_files/metronom.js
+++ src/Metronom_files/metronom.js
@@ -3,62 +3,62 @@
 //var achtel = 0;
 var takt = 0;
 var schlag = 0;
-var achtelDieserSchlag=0;
+var achtelDieserSchlag = 0;
 var timestamp = (new Date()).getTime();
 var achtelDauer = 200;
 
 var bars;
-var vorlauf=0;
+var vorlauf = 0;
 var achtelZeigen = true;
-var stopped= false;
+var stopped = false;
 
 var ersterBeep = true;
 
-function wechsleTempo(id){
+function wechsleTempo(id) {
 	var neueSatzNummer = document.getElementById("satz").value;
-		
-	if(neueSatzNummer==1){
-		document.getElementById("tempo").value=176;
+
+	if (neueSatzNummer == 1) {
+		document.getElementById("tempo").value = 176;
 	}
-	else if(neueSatzNummer==2){
-		document.getElementById("tempo").value=60;
+	else if (neueSatzNummer == 2) {
+		document.getElementById("tempo").value = 60;
 	}
-	else if(neueSatzNummer==3){
-		document.getElementById("tempo").value=140;
+	else if (neueSatzNummer == 3) {
+		document.getElementById("tempo").value = 140;
 	}
 }
-function start(){
+function start() {
 	ersterBeep = false;
 	erstenBeepAusloesen();
-	
-	stopped=true;
+
+	stopped = true;
 	setVariables();
 	//alert("start");
 	stopTimer();
-	stopped=false;
+	stopped = false;
 	run();
 	//setTimeout(jetztstarten, 5);
 }
 
-function jetztstarten(){
+function jetztstarten() {
 	//alert("start");
-	stopped=false;
+	stopped = false;
 	run();
 }
 
 
-function setVariables(){
-	var tempo=document.getElementById("tempo").value;
-	achtelDauer= (1000*60/2)/tempo;
-	
-	if(document.getElementById("achtelzeigen").checked) {
+function setVariables() {
+	var tempo = document.getElementById("tempo").value;
+	achtelDauer = (1000 * 60 / 2) / tempo;
+
+	if (document.getElementById("achtelzeigen").checked) {
 		achtelZeigen = true;
 	}
 	else {
 		achtelZeigen = false;
 	}
-	
-	var satzNr=document.getElementById("satz").value;
+
+	var satzNr = document.getElementById("satz").value;
 	if (satzNr == 1) {
 		bars = barsSatz1.slice(0);
 	}
@@ -68,53 +68,53 @@ function setVariables(){
 	if (satzNr == 3) {
 		bars = barsSatz3.slice(0);
 	}
-	
-	
-	takt=document.getElementById("starttakt").value-1;
-	
-	vorlauf=document.getElementById("vorlauf").value;
-	var startbar=bars[takt];
-	for (var i = 0; i<vorlauf; i++){
+
+
+	takt = document.getElementById("starttakt").value - 1;
+
+	vorlauf = document.getElementById("vorlauf").value;
+	var startbar = bars[takt];
+	for (var i = 0; i < vorlauf; i++) {
 		bars.splice(takt, 0, startbar);
 	}
-	
-	document.getElementById("aktuellertakt").value = takt+1-vorlauf;
-	
+
+	document.getElementById("aktuellertakt").value = takt + 1 - vorlauf;
+
 	// reset
-	
+
 	schlag = 0;
-	achtelDieserSchlag=0;
+	achtelDieserSchlag = 0;
 	timestamp = (new Date()).getTime();
-	
+
 }
 
 function run() {
-	
+
 	var now = (new Date()).getTime();
 
 	if (now - timestamp >= achtelDauer) {
-		if (schlag == 0 && achtelDieserSchlag==0){ // erster Achtel eines Taktes
+		if (schlag == 0 && achtelDieserSchlag == 0) { // erster Achtel eines Taktes
 			beep(10, 600, 50);
 		}
-		else if (achtelDieserSchlag==0){ // erster Achtel eines Schlages
+		else if (achtelDieserSchlag == 0) { // erster Achtel eines Schlages
 			beep(10, 500, 50);
 		}
-		else if(achtelZeigen) { // irgendein Achtel
+		else if (achtelZeigen) { // irgendein Achtel
 			beep(10, 400, 50);
 		}
 		timestamp = now;
-		
+
 		// hochzaehlen für nächsten Achtel
 		achtelDieserSchlag++;
 		if (achtelDieserSchlag >= bars[takt][schlag]) { // nicht mehr im schlag drin
 			// neuer Schlag
-			achtelDieserSchlag=0;
+			achtelDieserSchlag = 0;
 			schlag++;
-			if(schlag >= bars[takt].length){// nicht mehr im takt drin
-				schlag=0;
+			if (schlag >= bars[takt].length) {// nicht mehr im takt drin
+				schlag = 0;
 				takt++;
-				
-				document.getElementById("aktuellertakt").value = takt+1-vorlauf;
+
+				document.getElementById("aktuellertakt").value = takt + 1 - vorlauf;
 			}
 		}
 	}
@@ -133,40 +133,8 @@ function stopTimer() {
 }
 
 function stop() {
-	stopped=true;
+	stopped = true;
 }
-
-
-// https://forums.developer.apple.com/thread/48815#199210
-// angepasst:
-//document.addEventListener("DOMContentLoaded", function(event) {
-    // Do we have Web Audio API?
-//    try {
-//      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-//    } catch (e) {
-//      alert('Web Audio API is not supported in this browser');
-//    }
-//});
-
-
-
-// https://stackoverflow.com/questions/20054273/recording-audio-in-chrome-for-android-using-web-audio-api-and-navigator-getuserm
-//if (!window.AudioContext) {
-//        window.AudioContext = window.webkitAudioContext;
-//    }
-
-// // https://stackoverflow.com/questions/22977033/audiocontext-doesnt-exist-on-some-phones#22996293
-// var contextClass = (window.AudioContext || 
-// window.webkitAudioContext || 
-// window.mozAudioContext || 
-// window.oAudioContext || 
-// window.msAudioContext);
-// if (contextClass) {
-//     // Web Audio API is available.
-//     var context = new contextClass();
-// } else {
-//     // Web Audio API is not available. Fallback
-// }
 
 // Beep erstellen
 // https://stackoverflow.com/questions/35497243/how-to-make-a-short-beep-in-javascript-that-can-be-called-repeatedly-on-a-page#35498316
@@ -178,12 +146,12 @@ var v;
 var u;
 function erstenBeepAusloesen() {
 	// Do we have Audio API
-    try {
-      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    } catch (e) {
-      alert('Web Audio API is not supported in this browser');
-    }
-	
+	try {
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	} catch (e) {
+		alert('Web Audio API is not supported in this browser');
+	}
+
 	a = new AudioContext();
 	//v = a.createOscillator();
 	//u = a.createGain();
@@ -196,14 +164,14 @@ function erstenBeepAusloesen() {
 function beep(loud, freq, dur) {
 	//var a = new AudioContext();
 	//console.log(loud+x+y)
-	
+
 	// notwendig?
 	v = a.createOscillator();
 	u = a.createGain();
 	v.connect(u);
 	v.type = "square";
 	u.connect(a.destination);
-	
+
 	v.frequency.value = freq;
 	u.gain.value = loud * 0.01;
 	v.start(a.currentTime);
@@ -214,21 +182,21 @@ function beep(loud, freq, dur) {
 // https://stackoverflow.com/questions/3762589/fastest-javascript-summation
 function sumArray(myArray) {
 	var count = 0;
-	for (var i = myArray.length-1; i>0; i--) {
+	for (var i = myArray.length - 1; i > 0; i--) {
 		count += myArray[i];
 	}
 	return count;
 }
 
 var barsSatz1 = [
-[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2],[2,2,2,2]
+	[2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]
 ];
 
 var barsSatz2 = [
-[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2],[2,2,2]
+	[2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2]
 ];
 
 var barsSatz3 = [
-	[2,2,2,2],[2,2,2,2],[2,2,2,3],[2,2,3],[2,2,3],[2,2,3],[2,2,2,3],[2,2,3],[2,2,2,3],[2,2,3],[2,2,2,3],[3,3,2],[3,3,2],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[2,2,3],[2,2,2,3],[2,2,3],[2,2,2,3],[2,2,3,3],[3],[3],[3],[3],[3],[3],[2,2,3],[2,2,3],[2,2,3],[3,3,2],[2,2,3],[2,2,3],[2,2,3],[3,3,2],[2,2,3],[2,2,3],[3,3,3],[3,3,3],[2,2,3],[2,2,2,3],[2,2,3],[2,2,2,3],[2,2,3],[2,2,2,2,3],[3],[3],[3],[3],[2,2,3],[2,2,3],[2,2,3,3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3,2],[3,2],[3,2],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[2,2,3],[2,2,2,3],[2,2,3],[2,2,2,3],[2,2,3],[2,2,2,2,3],[2,2,3],[2,2,2,3],[2,2,3],[2,2,2,3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[2,2,3],[3],[3],[3],[3]
+	[2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 3], [2, 2, 3], [2, 2, 3], [2, 2, 3], [2, 2, 2, 3], [2, 2, 3], [2, 2, 2, 3], [2, 2, 3], [2, 2, 2, 3], [3, 3, 2], [3, 3, 2], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [2, 2, 3], [2, 2, 2, 3], [2, 2, 3], [2, 2, 2, 3], [2, 2, 3, 3], [3], [3], [3], [3], [3], [3], [2, 2, 3], [2, 2, 3], [2, 2, 3], [3, 3, 2], [2, 2, 3], [2, 2, 3], [2, 2, 3], [3, 3, 2], [2, 2, 3], [2, 2, 3], [3, 3, 3], [3, 3, 3], [2, 2, 3], [2, 2, 2, 3], [2, 2, 3], [2, 2, 2, 3], [2, 2, 3], [2, 2, 2, 2, 3], [3], [3], [3], [3], [2, 2, 3], [2, 2, 3], [2, 2, 3, 3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3, 2], [3, 2], [3, 2], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [2, 2, 3], [2, 2, 2, 3], [2, 2, 3], [2, 2, 2, 3], [2, 2, 3], [2, 2, 2, 2, 3], [2, 2, 3], [2, 2, 2, 3], [2, 2, 3], [2, 2, 2, 3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [3], [2, 2, 3], [3], [3], [3], [3]
 
 ];
