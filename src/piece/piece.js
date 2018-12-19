8acc0e5a02743d5a42b8f8709824e2a3da694ac5
Christoph Raaflaub <raaflaub@puzzle.ch>
2018-12-19 21:46:34 +0100

    simplify masquerade definition with bars config

--- src/piece/piece.js
+++ src/piece/piece.js
@@ -28,7 +28,11 @@ Piece.prototype.addParts = function(parts) {
   for (var i = 0; i < size; i++) {
     part = parts[i];
     if (part instanceof Bars) {
-      // this.parts.concat(part);
+      var barsPart = [part.amount];
+      for (var j = 0; j < part.amount; j++) {
+        barsPart[j] = part.content;
+      }
+      this.parts[i] = barsPart;
     } else {
       this.parts[i] = part;
     }
