7017a87a8429f36dcca1088e3a3e7904fe043de6
Christoph Raaflaub <raaflaub@puzzle.ch>
2018-12-20 14:06:39 +0100

    check every part item for Bars config

--- src/piece/piece.js
+++ src/piece/piece.js
@@ -27,14 +27,34 @@ Piece.prototype.addParts = function(parts) {
   var part;
   for (var i = 0; i < size; i++) {
     part = parts[i];
+
     if (part instanceof Bars) {
+      // whole part is defined by Bars config
       var barsPart = [part.amount];
       for (var j = 0; j < part.amount; j++) {
         barsPart[j] = part.content;
       }
       this.parts[i] = barsPart;
     } else {
-      this.parts[i] = part;
+      // part is an array
+      var part2 = [];
+      var subPart2;
+
+      for (var k = 0; k < part.length; k++) {
+        subPart2 = part[k];
+
+        if (subPart2 instanceof Bars) {
+          var barsSubPart2 = [subPart2.amount];
+          for (var l = 0; l < subPart2.amount; l++) {
+            barsSubPart2[l] = subPart2.content;
+          }
+          part2.concat(barsSubPart2);
+        } else {
+          part2[k] = subPart2;
+        }
+      }
+
+      this.parts[i] = part2;
     }
   }
 }
